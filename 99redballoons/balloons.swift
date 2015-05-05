a03ceb8d7e0cfc9cedaae991ff5134a466fe8d8c
rcjones <rcjones.iphone@gmail.com>
2015-05-05 18:29:33 -0500

    added Balloons struct

---
+++ 99redballoons/balloons.swift
@@ -0,0 +1,40 @@
+//
+//  balloons.swift
+//  99redballoons
+//
+//  Created by Ryan Jones on 5/5/15.
+//  Copyright (c) 2015 Ryan Jones. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloons {
+    var number:Int
+    var image:UIImage(named:"")
+    
+    func number() -> Int {
+        let randomNumber = Int(arc4random_uniform(UInt32(100)))
+        return randomNumber
+    }
+    func image() -> String {
+        let randomNumber = Int(arc4random_uniform(UInt32(5)))
+        var randomImage: String
+        
+        switch randomNumber {
+        case 0:
+            randomImage = "BerlinTVTower.jpg"
+        case 1:
+            randomImage = "RedBalloon1.jpg"
+        case 2:
+            randomImage = "RedBalloon2.jpg"
+        case 3:
+            randomImage = "RedBalloon3.jpg"
+        default:
+            randomImage = "RedBalloon4.jpg"
+        }
+        return randomImage
+        
+    }
+}
+
